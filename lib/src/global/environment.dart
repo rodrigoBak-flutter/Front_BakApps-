f6bf2c750a8ef374099e02b9b0edf64c853d241e
Rodrigo <rodrigosebastianbak@hotmail.com>
2022-12-21 10:20:50 +0100

    app_authNode_bakApps_terminado

--- lib/src/global/environment.dart
+++ lib/src/global/environment.dart
@@ -1,9 +1,9 @@
 
 
 class Environment {
-  // static String apiUrl    = Platform.isAndroid ? 'http://192.168.1.137/api' : 'http://192.168.1.137/api';
-  // static String socketUrl = Platform.isAndroid ? 'http://192.168.1.137:3000'     : 'http://192.168.1.137';
+  // static String apiUrl    = Platform.isAndroid ? 'http:///api' : 'http:///api';
+  // static String socketUrl = Platform.isAndroid ? 'http://:3000'     : 'http://.137';
 
-  static String apiUrl = 'http://192.168.1.137:3000/api';
-  static String socketUrl = 'http://192.168.1.137:3000/';
+  static String apiUrl = 'http://<TU IP>/api';
+  static String socketUrl = 'http://<TU IP>:3000/';
 }
